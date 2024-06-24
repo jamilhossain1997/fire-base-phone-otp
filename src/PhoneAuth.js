815dd01ebbad312c89cb0d3674e8f64661bb26d9
jamil.hossain <jamil.hossain@nexdecade.com>
2024-06-24 12:15:49 +0600

    phoneAuth---verifiy working

--- src/PhoneAuth.js
+++ src/PhoneAuth.js
@@ -1,57 +1,47 @@
-import React, { useState } from 'react';
-import { RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
-import PhoneInput from 'react-phone-input-2';
-import 'react-phone-input-2/lib/style.css';
-import { auth } from './firebase';
-
+import React, { useState } from "react";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import { auth } from "./firebase";
 
 function PhoneAuth() {
+  const [phone, setphone] = useState("");
+  const [user, setUser] = useState(null);
+  const [otp, setOtp] = useState("");
 
-    const [phone,setphone]=useState("");
-    const [user,setUser]=useState(null);
-    const [otp,setOtp]=useState("");
-
-    const sendOtp = async ()=>{
-        try{
-            const recaptcha = new RecaptchaVerifier(auth,"recaptcha",{});
-            const confirmation = await signInWithPhoneNumber(auth,phone,recaptcha);
-            setUser(confirmation);
-            
-        }catch(err){
-            console.error(err);
-        }
-        
+  const sendOtp = async () => {
+    try {
+    //   const recaptcha = new RecaptchaVerifier(auth, "recaptcha", {});
+      const confirmation = await signInWithPhoneNumber(auth, phone);
+      setUser(confirmation);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-
-    const verfiyOtp=async()=>{
-
-        try{
-
-        }catch(err){
-            console.error(err)
-        }
-       await  user.confirm();
+  const verfiyOtp = async () => {
+    try {
+      await user.confirm(otp);
+    } catch (err) {
+      console.error(err);
     }
-    
+  };
 
   return (
     <div>
-        <PhoneInput
-        country={'bd'}
+      <PhoneInput
+        country={"bd"}
         value={phone}
-        onChange={()=>setphone("+"+phone)}
-        />
+        onChange={(phone) => setphone("+" + phone)}
+      />
 
-        <button onClick={sendOtp}>SEND OTP</button> <br/><br/>
-
-        <input 
-           onChange={(e)=>setOtp(e.target.value)}    
-        />
-
-    <button onClick={verfiyOtp}>verfiy Otp</button>
+      <div id="recaptcha"></div>
+      <button onClick={sendOtp}>SEND OTP</button> <br />
+      <br />
+      <input onChange={(e) => setOtp(e.target.value)} />
+      <button onClick={verfiyOtp}>verfiy Otp</button>
     </div>
-  )
+  );
 }
 
-export default PhoneAuth
+export default PhoneAuth;
